6f6e35c251761c5f2e45391406a288d28d38b6d1
xiaoheng20190505 <1912272879@qq.com>
2020-07-07 00:33:57 +0800

    验证v-for 结合 组件 动态渲染

---
+++ Vue学习/Day_01/v-model详解/动态渲染自定义组件/README.md
@@ -0,0 +1,3 @@
+## INTRODUCTION
+
+实现动态渲染组件
---
+++ Vue学习/Day_01/v-model详解/动态渲染自定义组件/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="templateContainer">
+       <p>我是一个小方块，下面是我的值</p>
+       <p>{{ data.name }}</p>
+    </div>
+</template>
+<script>
+module.exports = {
+    name: 'mySelect',
+    props: {
+        data: {
+            type: Object,
+            default: () => {
+                return {}
+            }
+        }
+    },
+    data(){
+        return {}
+    }
+}
+</script>
+<style>
+.templateContainer {
+    border: 1px solid #3598db;
+    margin: 10px 0;
+}
+</style>
